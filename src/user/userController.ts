bac9de3ec5e8794d7befd652b97eaec130ca784c
RRRishav <rishav6787@gmail.com>
2025-07-16 01:41:02 +0530

    userLogin created succesfully api testing is successful

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -6,6 +6,12 @@ import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
 import {sign} from 'jsonwebtoken'
 import dotenv from 'dotenv';
+import { access } from "fs";
+
+
+
+
+
 dotenv.config();
 
 const createUser = async (req:Request, res:Response,next:NextFunction) => {
@@ -20,6 +26,8 @@ const createUser = async (req:Request, res:Response,next:NextFunction) => {
     }
     
     //Database call
+
+
 
     const User = await userModel.findOne({
         email
@@ -67,4 +75,52 @@ res.status(201).json({
 
 }
 
-export {createUser};
+
+
+const loginUser = async (req:Request, res:Response,next:NextFunction) => {
+
+const { email, password } = req.body;
+
+if(!email || !password) {
+    const error = createHttpError(400, "Please provide both email and password.");
+    return next(error);
+}
+
+//Database call
+const user = await userModel.findOne({
+    email
+});
+
+if(!user) {
+    const error = createHttpError(404, "User not found with this email.");
+    return next(error);
+}
+
+const isMatch = await bcrypt.compare(password, user.password);
+
+if(!isMatch) {
+    const error = createHttpError(401, "Invalid password.");
+    return next(error);
+}
+
+
+//Token generation
+const token = sign({
+    sub: user._id,
+}, process.env.JWT_SECRET as string, {
+    expiresIn: '1d'
+});
+
+
+
+
+res.status(200).json({
+    message: "Login successful" ,accessToken: token,
+})
+
+}
+
+    
+
+
+export {createUser, loginUser};
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,11 +1,12 @@
 import { create } from "domain";
 import express from "express"
-import { createUser } from "./userController";
+import { createUser ,loginUser} from "./userController";
 
 const userRouter = express.Router();
 //Adding Routes for my project 
 userRouter.post('/register',createUser);    
 
+userRouter.post('/login', loginUser)
 
 
 export default userRouter;
