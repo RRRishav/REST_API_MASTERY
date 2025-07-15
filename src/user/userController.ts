6498fa4eb5c9fe0662cfb238d5cafd1d67a2ef45
RRRishav <rishav6787@gmail.com>
2025-07-16 00:57:16 +0530

    add register user validtaion

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,8 +1,26 @@
 import { NextFunction } from "express";
 import { Request, Response } from "express";
+import createHttpError from "http-errors";
 
 
 const createUser = async (req:Request, res:Response,next:NextFunction) => {
+    const { name, email, password } = req.body;
+
+    //Validation
+
+    if(!name || !email || !password) {
+        const error = createHttpError(400, "Please provide all required fields: name, email, and password.");
+        return next(error);
+       
+    }
+    
+   
+
+    //process
+
+    //Resonse
+
+
 
 
 res.status(201).json({
